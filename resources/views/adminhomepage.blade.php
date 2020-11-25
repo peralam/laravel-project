cd131bc804735401da8d3d6e4b3c43c8a56beb06
peralam <midhu997@gmail.com>
2020-11-25 07:39:24 -0800

    first commit

---
+++ resources/views/adminhomepage.blade.php
@@ -0,0 +1,39 @@
+<html>
+    <head>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" 
+        integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+
+        <title>Administrator Home Page</title>
+        <link href="adminhomepge.css" type="text/css" rel="stylesheet">
+    </head>
+    <body>
+        <div class="header">
+            <h1>Exam Administrator</h1>
+        </div>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+           
+            
+            <div class="collapse navbar-collapse" id="navbarNav">
+              <ul class="navbar-nav">
+                <li class="nav-item active">
+                  <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">Student Details</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">Exam Details</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Instructor Details</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#">Log Out</a>
+                  </li>
+               
+              </ul>
+            </div>
+          </nav>
+
+    </body>
+</html>
